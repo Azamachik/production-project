8a668d386718bc67aa0fe8fb2cc999d424924cdc
Azamat <azamat-karimov-05@mail.ru>
2025-12-08 01:30:41 +0300

    Initial commit

---
+++ src/components/Counter.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from './Counter.module.scss';
+
+export const Counter = () => {
+  const [count, setCount] = React.useState(0);
+
+  return (
+    <div className={classes.button}>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>Увеличить</button>
+    </div>
+  );
+};
+
